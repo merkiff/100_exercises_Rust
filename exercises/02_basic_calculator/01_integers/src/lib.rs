60b75eb3d498c9e16e97b202f019de084d7c8d49
merkiff <minseong1220@gmail.com>
2024-06-24 15:38:01 +0900

    02_basic_calculator done

--- exercises/02_basic_calculator/01_integers/src/lib.rs
+++ exercises/02_basic_calculator/01_integers/src/lib.rs
@@ -1,6 +1,7 @@
 fn compute(a: u32, b: u32) -> u32 {
     // TODO: change the line below to fix the compiler error and make the tests pass.
-    a + b * 4u8
+    //a와 b가 u32이므로 4 역시 u32 타입이어야 함
+    a + b * 4u32
 }
 
 #[cfg(test)]
--- exercises/02_basic_calculator/02_variables/src/lib.rs
+++ exercises/02_basic_calculator/02_variables/src/lib.rs
@@ -8,7 +8,8 @@
 pub fn speed(start: u32, end: u32, time_elapsed: u32) -> u32 {
     // TODO: define a variable named `distance` with the right value to get tests to pass
     //  Do you need to annotate the type of `distance`? Why or why not?
-
+    //end와 start가 u32이므로, 이 값만을 이용한 변수 distance는 컴파일러가 자동으로 타입을 추론할 수 있을 것
+    let distance = end - start;
     // Don't change the line below
     distance / time_elapsed
 }
