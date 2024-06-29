bad738e91b8510f5ebd73ee622e6101205da2e8b
merkiff <minseong1220@gmail.com>
2024-06-29 21:33:55 +0900

    04 done

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,8 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
+use std::ops::Add;
 
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -11,6 +13,13 @@ impl WrappingU32 {
     }
 }
 
+impl Add for WrappingU32 {
+    type Output = Self;
+    
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.value.wrapping_add(rhs.value))
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
